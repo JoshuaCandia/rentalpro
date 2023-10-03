bf57287e2909633c739e0d886e8f893bd06f655a
Joshua Candia <joshuacandia74@gmail.com>
2023-10-03 20:07:07 -0300

    feat(Client/assets): pics added}

--- rentalpro/tailwind.config.js
+++ rentalpro/tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = withMT({
     './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        max: '1920px',
+      },
+    },
   },
   plugins: [],
 })
